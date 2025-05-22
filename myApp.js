f74a6467cd8cc660e3677e5acd5372f2bc381236
Syan <torressyanvale@gmail.com>
2025-05-22 10:09:53 +0800

    re

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://elysian:127380728Bsafe@cluster0.1b7moga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected successfully'))
   .catch(err => console.error('Connection error:', err));
 
