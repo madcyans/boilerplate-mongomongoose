c2e462947d2a915c695188afd58924f5d039bf03
Syan <torressyanvale@gmail.com>
2025-05-22 10:14:36 +0800

    re

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.connectMONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected successfully'))
   .catch(err => console.error('Connection error:', err));
 
