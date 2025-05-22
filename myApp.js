2a0f9b80526da98d107ed9ea56e859eb15da3ea7
Syan <torressyanvale@gmail.com>
2025-05-22 14:38:41 +0800

    re

--- myApp.js
+++ myApp.js
@@ -4,7 +4,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('Database connected successfully'))
   .catch(err => console.error('Connection error:', err));
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number },
+  favoriteFoods: { type: [String] }
+});
+
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
